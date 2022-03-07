2c218bb4907f02401997756e30eb11e17cb74d6f
NicolasMuller19 <nicolasmatiasmuller19@gmail.com>
2022-03-07 20:11:41 -0300

    first commit

---
+++ Javascript/burguerMenu.js
@@ -0,0 +1,7 @@
+const burg= document.querySelector('.burguer_icono');
+
+const links=document.querySelector('.header_nav_div-links_contenedor');
+
+burg.addEventListener('click', ()=> {
+    links.classList.toggle('active');
+})
